135a79798791fb8ea9c6b2fe81c875288646575f
Your Name <you@example.com>
2026-02-12 11:10:40 -0300

    aula1

---
+++ entrada.py
@@ -0,0 +1,13 @@
+print()
+print()
+name = input("Nome: ")
+print()
+senha = input("Senha: ")
+print()
+dominio = input("Domínio: ")
+
+print()
+print()
+
+print ('Suas informações:'+'\n'+'\n'+'Nome: ' + name + ' - Senha: ' + senha + ' - Domínio: '+ dominio +'\n'+'\n'+'Bem-vindo ao Python!!!!!!!!!!!!!!!!' +'\n'+'\n' + "O seu email é: "+ name+senha+"@"+dominio+".com"+'\n'+'\n')
+
