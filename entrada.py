5f3220d2b5e8723207bf8731c833918bebf5abd0
Your Name <you@example.com>
2026-02-19 10:54:58 -0300

    terceiro commit

--- entrada.py
+++ entrada.py
@@ -2,21 +2,39 @@ print()
 print()
 name = input("Nome: ")
 print()
-senha = input("Senha: ")
-print()
 dominio = input("Domínio: ")
 
 print()
 print()
 
-print ('Suas informações:'+'\n'+'\n'+'Nome: ' + name + ' - Senha: ' + senha + ' - Domínio: '+ dominio +'\n'+'\n'+'Bem-vindo ao Python!!!!!!!!!!!!!!!!' +'\n'+'\n' + "O seu email é: "+ name+senha+"@"+dominio+".com"+'\n'+'\n')
+print ('Suas informações:'+'\n'+'\n'+'Nome: ' + name + ' - Domínio: '+ dominio +'\n'+'\n'+'Bem-vindo ao Python!!!!!!!!!!!!!!!!' +'\n'+'\n' + "O seu email é: "+ name.lower()+"@"+dominio.lower()+".com"+'\n'+'\n')
+email = name+dominio+str('com')
 
 print()
-palavra = "jaca"
+
+#colocar string com letras minúsculas
+
+palavra = name
+
 #colocar string com letras maíusculas 
+
 print('Colocando o texto em letra maíuscula: '+palavra.upper())
 print()
-PALAVRA = 'JACA'
+PALAVRA = name
 print('Colocando o texto em letra minúscula: '+palavra.lower())
 print()
 
+#Gerar senha automaticamente pelo contador de string
+
+print ("Sua senha automática a partir do seu email:")
+a = email.count('a')
+e = email.count('e')
+i = email.count('i')
+o = email.count('o')
+u = email.count('u')
+
+print()
+
+print("Senha: a"+str(a)+"e"+str(e)+"i"+str(i)+"o"+str(o)+"u"+str(u))
+
+print()
