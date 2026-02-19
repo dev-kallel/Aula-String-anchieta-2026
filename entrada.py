e0573068bc86078755b1db7984e412db9549b978
Your Name <you@example.com>
2026-02-19 09:46:16 -0300

    segundo commit

--- entrada.py
+++ entrada.py
@@ -11,3 +11,12 @@ print()
 
 print ('Suas informações:'+'\n'+'\n'+'Nome: ' + name + ' - Senha: ' + senha + ' - Domínio: '+ dominio +'\n'+'\n'+'Bem-vindo ao Python!!!!!!!!!!!!!!!!' +'\n'+'\n' + "O seu email é: "+ name+senha+"@"+dominio+".com"+'\n'+'\n')
 
+print()
+palavra = "jaca"
+#colocar string com letras maíusculas 
+print('Colocando o texto em letra maíuscula: '+palavra.upper())
+print()
+PALAVRA = 'JACA'
+print('Colocando o texto em letra minúscula: '+palavra.lower())
+print()
+
